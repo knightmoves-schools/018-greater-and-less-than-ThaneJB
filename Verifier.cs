ae7e96f918a2edf8230d9c3ed1d600e77391f6ea
Thane <thabay0606@students.maquoketaschools.org>
2026-03-02 14:25:51 -0600

    Added all methods.

--- Verifier.cs
+++ Verifier.cs
@@ -2,4 +2,31 @@
 public class Verifier
 {
     // Add your code here
+    public string OldEnough(int age){
+        if (age > 17){
+            return "old enough";
+        }
+            return "";
+    }
+    
+    public string UnderAge(int age){
+        if (age < 18){
+            return "under age";
+        }
+            return "";
+    }
+    
+    public string LegalSpeed(int speed){
+        if (speed <= 50){
+            return "legal speed";
+        }
+            return "";
+    }
+    
+    public string GoldenYears(int age){
+        if (age >= 65){
+            return "golden years";
+        }
+            return "";
+    }
 }
